86c704ab1314f6596ee9650b902734cd8ce8914c
TiavinaMichael <ralainirinatiavina@gmail.com>
2024-06-29 06:59:40 +0000

    update lib

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import {
   Tabs,
   ThemeProvider,
   createTheme,
+  Box,
+  FormControlLabel,
+  Switch,
 } from "@mui/material";
 import { SyntheticEvent, useEffect, useState } from "react";
 import { TextEditor, TextEditorReadOnly } from "mui-tiptap-editor";
@@ -13,16 +16,84 @@ import TiptapParser from "tiptap-parser";
 import WithHookForm from "./WithHookForm";
 import Footer from "./Footer";
 
+const customLabels = {
+  editor: {
+    editor: "Editeur",
+    preview: "Aperçu"
+  },
+  toolbar: {
+    bold: "Gras",
+    italic: "Italique",
+    strike: "Barré",
+    underline: "Souligné",
+    link: "Lien",
+    bulletList: "Liste à puces",
+    orderedList: "Liste ordonnée",
+    alignLeft: "Aligner à gauche",
+    alignCenter: "Aligner au centre",
+    alignRight: "Aligner à droite",
+    alignJustify: "Justifier",
+    blockquote: "Citation",
+    codeBlock: "Code",
+    table: "Table",
+    youtube: "Youtube",
+    undo: "Annuler",
+    redo: "Refaire",
+    mention: "Mention"
+  },
+  headings: {
+    normalText: "Text normal",
+    h1: "En-tête 1",
+    h2: "En-tête 2",
+    h3: "En-tête 3",
+    h4: "En-tête 4",
+    h5: "En-tête 5",
+    h6: "En-tête 6"
+  },
+  table: {
+    table: "Tableau",
+    addColumnBefore: "Ajouter une colonne avant",
+    addColumnAfter: "Ajouter une colonne après",
+    deleteColumn: "Supprimer la colonne",
+    addRowBefore: "Ajouter une ligne avant",
+    addRowAfter: "Ajouter une ligne après",
+    deleteRow: "Supprimer la ligne",
+    mergeCells: "Fusionner les cellules",
+    splitCell: "Diviser la cellule",
+    deleteTable: "Supprimer le tableau",
+    insertTable: "Insérer un tableau",
+    toggleHeaderCell: "Basculer la cellule d'en-tête",
+    toggleHeaderColumn: "Basculer la colonne d'en-tête",
+    toggleHeaderRow: "Basculer la ligne d'en-tête",
+    mergeOrSplit: "Fusionner ou diviser",
+    setCellAttribute: "Définir l'attribut de cellule"
+  },
+  link: {
+    link: "Lien",
+    insert: "Insérer le lien",
+    invalid: "Lien invalide",
+  },
+  youtube: {
+    link: "Lien",
+    insert: "Insérer la vidéo Youtube",
+    title: "Insérer une vidéo Youtube",
+    invalid: "Lien invalide",
+    enter: "Entrer le lien",
+    height: "Hauteur",
+    width: "Largeur"
+  }
+}
 const tabs = [
-  "Simple",
-  "Toolbar",
-  "Read only",
-  "Custom global styles",
-  "Each element styles",
-  "Mentions",
-  "Async initial value",
-  "React Hook Form",
-  "Read without editor",
+  'Simple',
+  'Selected Toolbar',
+  'Read only',
+  'Custom global styles',
+  'Each element styles',
+  'Override labels',
+  'Mentions',
+  'Async initial value',
+  'React Hook Form',
+  'Read without editor'
 ];
 
 const mentions = [
@@ -71,6 +142,15 @@ const delay = (time: number) =>
   new Promise((resolve) => {
     setTimeout(resolve, time);
   });
+  
+const getTheme = (mode: 'light' | 'dark') => createTheme({
+  palette: {
+    mode,
+    },
+  });
+
+const defaultMode = 'light';
+
 const htmlToParse = `
 <h1>Display the content without to install the editor</h1>
 <p>Use <a href="https://www.npmjs.com/package/tiptap-parser">tiptap-parse</a> for that</p>
@@ -91,90 +171,116 @@ const htmlToParse = `
 
 const App = () => {
   const [tab, setTab] = useState<number>(0);
-  const [asyncDefaultValue, setAsyncDefaultValue] = useState<string>("");
+  const [asyncDefaultValue, setAsyncDefaultValue] = useState<string>('');
+  const [mode, setMode] = useState<'light' | 'dark'>(defaultMode);
 
   // load async default value
   useEffect(() => {
     const fetchData = async () => {
-      await delay(1000);
-      setAsyncDefaultValue("<p>Initial value from API for example</p>");
-    };
+      await delay(1000)
+      setAsyncDefaultValue('<p>Initial value from API for example</p>')
+    }
     fetchData();
-  }, []);
+  }, [])
+
+  const handleChangeMode = () => {
+    setMode(mode === 'light' ? 'dark' : 'light');
+  }
 
   const handleChange = (_: SyntheticEvent, newValue: number) => {
     setTab(newValue);
   };
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={getTheme(mode)}>
       <CssBaseline />
-      {/* ---------------- tabs ---------------- */}
-      <Container maxWidth="xl" className="spaceBetween">
-        <Tabs
-          value={tab}
-          onChange={handleChange}
-          aria-label="tabs"
-          variant="scrollable"
-          scrollButtons="auto"
-          sx={{ mb: 2 }}
-        >
-          {tabs.map((label, index) => (
-            <Tab key={index} label={label} value={index} />
-          ))}
-        </Tabs>
-      </Container>
-      {/* ------------- tabs panels ------------- */}
-      <Container sx={{ mt: 2, minHeight: "60vh" }}>
-        {/* Simple input */}
-        {tab === 0 && <TextEditor placeholder="Type something here..." />}
+        <Box sx={{ bgcolor: 'background.paper', display: 'flex' }}>
+          {/* -------------------------------------- */}
+          {/* ---------------- tabs ---------------- */}
+          {/* -------------------------------------- */}
+          <Tabs value={tab} onChange={handleChange} aria-label="tabs"sx={{ mb: 2 }}  orientation="vertical" variant="scrollable">
+            {tabs.map((label, index) => (
+              <Tab key={index} label={label} value={index} sx={{ textAlign: 'left', alignItems: 'start' }} />
+            ))}
+          </Tabs>
 
-        {/* Select toolbar */}
-        {tab === 1 && (
-          <TextEditor
-            placeholder="Type something here..."
-            toolbar={["bold", "italic", "underline"]}
-          />
-        )}
-        {/* Read only */}
-        {tab === 2 && <TextEditorReadOnly value="<p>Hello word!</p>" />}
-        {/* Custom global styles */}
-        {tab === 3 && (
-          <TextEditor value="<p>Hello word!</p>" rootClassName="root" />
-        )}
-        {/* Each element styles */}
-        {tab === 4 && (
-          <TextEditor
-            value="<p>Hello word!</p>"
-            label="Content"
-            tabClassName="my-tab"
-            labelClassName="my-label"
-            inputClassName="my-input"
-            toolbarClassName="my-toolbar"
-          />
-        )}
+          {/* --------------------------------------- */}
+          {/* ------------- tabs panels ------------- */}
+          {/* --------------------------------------- */}
+          <Container sx={{ mt: 1 }}>
+            {/* mode switch */}
+            <Box sx={{ mt: 3 }}>
+              <FormControlLabel
+                control={
+                  <Switch checked={mode === defaultMode} onChange={handleChangeMode} />
+                }
+                label={mode}
+                sx={{ textTransform: 'capitalize' }}
+              />
+            </Box>
+            {/* Simple input */}
+            {tab === 0 && <TextEditor placeholder='Type something here...' />}
 
-        {/* mentions */}
-        {tab === 5 && (
-          <TextEditor
-            label="Content"
-            mentions={mentions}
-            user={currentUser}
-            userPathname="/profile"
-          />
-        )}
-        {/* With default async value */}
-        {tab === 6 && <TextEditor value={asyncDefaultValue} />}
+            {/* Select toolbar */}
+            {tab === 1 && (
+              <TextEditor
+                placeholder='Type something here...'
+                toolbar={['bold', 'italic', 'underline']}
+              />
+            )}
+            {/* Read only */}
+            {tab === 2 && <TextEditorReadOnly value="<p>Hello word!</p>" />}
+            {/* Custom global styles */}
+            {tab === 3 && (
+              <TextEditor
+                value="<p>Hello word!</p>"
+                rootClassName="root"
+              />
+            )}
+            {/* Each element styles */}
+            {tab === 4 && (
+              <TextEditor
+                value="<p>Hello word!</p>"
+                label="Content"
+                tabClassName="my-tab"
+                labelClassName="my-label"
+                inputClassName="my-input"
+                toolbarClassName="my-toolbar"
+              />
+            )}
 
-        {/* With React Hook Form */}
-        {tab === 7 && <WithHookForm />}
+            {/* Override labels */}
+            {tab === 5 && (
+              <TextEditor
+                label="Content"
+                labels={customLabels}
+              />
+            )}
 
-        {/* Read without editor */}
-        {tab === 8 && <TiptapParser content={htmlToParse} />}
-      </Container>
+            {/* mentions */}
+            {tab === 6 && (
+              <TextEditor
+                label="Content"
+                mentions={mentions}
+                user={currentUser}
+                userPathname="/profile"
+              />
+            )}
+            {/* With default async value */}
+            {tab === 7 && <TextEditor value={asyncDefaultValue} />}
+
+            {/* With React Hook Form */}
+            {tab === 8 && <WithHookForm />}
+
+            {/* Read without editor */}
+            {tab === 9 && (
+              <TiptapParser content={htmlToParse} />
+            )}
+          </Container>
+      </Box>
       <Footer />
     </ThemeProvider>
-  );
+  )
 };
 
 export default App;
